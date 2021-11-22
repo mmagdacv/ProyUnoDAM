f5d5e2b4166edec317a5fdbe047b10f0b5ee98a4
María Magdalena Carril <mmcarril@ateis.local>
2021-11-22 18:58:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from ventana import *
+import sys
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication([])
+    window = Main()
+    window.show()
+    sys.exit(app.exec())
+
+
